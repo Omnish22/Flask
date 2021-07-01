393248fd7bbe7b14390d96f8ced8d96e20b9931a
Omnish Kumar <omnish22@gmail.com>
2021-07-01 22:23:37 +0530

    mongodb added

--- 9_flask/microBlog/app.py
+++ 9_flask/microBlog/app.py
@@ -1,10 +1,14 @@
 from flask import Flask , render_template, request 
 import datetime
+from pymongo import MongoClient
 
 app = Flask(__name__)
+client = MongoClient('mongodb://127.0.0.1:27017/')
+app.db = client['miniBlogPost']
 
 
-data = {}
+
+# data = {}
 @app.route("/", methods=['GET','POST'])
 def home():
     if request.method=="GET":
@@ -16,12 +20,16 @@ def home():
             title = request.form.get('title')
             content = request.form.get('content')
             date = datetime.datetime.today().date()
-            time = datetime.datetime.today().time()
-            data[title] = [content,date,time]
+            time = datetime.datetime.today().time().strftime("%H:%M")
+            # data[title] = [content,date,time]
 
+            app.db.data.insert({"title":title,"content":content,"date":f"{date}","time":f"{time}"})
+
+    data={entry['title']:[entry['content'],entry['date'],entry['time']] for entry in app.db.data.find({})}
+    
+    # print("database: " ,app.db.data.find({}))
+    # print([data for data in app.db.data.find({})])
             
-
-            return render_template('index.html',data=dict(reversed(list(data.items()))))
     return render_template("index.html",data=dict(reversed(list(data.items()))))
 
 if __name__=="__main__":
