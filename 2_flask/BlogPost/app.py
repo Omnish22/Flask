98a42525a497a097488563cd04169ca942ea9edf
Omnish Kumar <omnish22@gmail.com>
2021-07-13 22:53:37 +0530

    BlogPost home page done

--- 2_flask/BlogPost/app.py
+++ 2_flask/BlogPost/app.py
@@ -1,23 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session
 from database import Database
 from models.post import Post 
 from models.blog import Blog
 from models.users import User
-# from typing_extensions import ParamSpec
 
 Database.initialize()
-
-
-
-
-
 
 app = Flask(__name__)
 
 
+
+
 @app.route("/")
 def home():
-    return render_template("home.html")
+    # print("session: ",session)
+    blogs = [Blog.getAllBlogs()[len(Blog.getAllBlogs())-1-i] for i in range(len(Blog.getAllBlogs()))]
+    return render_template("home.html",sess=session,blogs = blogs,nblogs=len(blogs))
+
+# @app.login("/login")
+# def login():
+
 
 
 if __name__ == "__main__":
